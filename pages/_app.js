aa5745b30cae5da39dbbcaec502945576f59d906
Fabliha Hossain <fablihahossain27@gmail.com>
2021-02-22 18:33:34 -0500

    installed and tested reacstrap, emotion, and bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
