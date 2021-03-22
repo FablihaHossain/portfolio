4dbf8c27cddd2f784634f4a965253c4924a7e011
Fabliha Hossain <fablihahossain27@gmail.com>
2021-03-22 17:48:34 -0400

    testing out new components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,42 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import MyNavbar from '../components/MyNavbar'
+import ProjectCard from '../components/ProjectCard'
+import { Row } from 'reactstrap';
+import styled from '@emotion/styled';
+
+const ProjectDeck = styled(Row)({
+  display: 'grid',
+  gridTemplateColumns: ' auto auto auto',
+  backgroundColor: '#ff884d'
+});
 
 export default function Home() {
   return (
     <>
-      <div>
-        <h5> This is my portfolio</h5>
-      </div>
+      <MyNavbar></MyNavbar>
+      <ProjectDeck>
+        <ProjectCard
+          title="Faveart"
+          logo="/Images/FaveartLogo.png"
+          github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+          description="senior capstone"
+        ></ProjectCard>
+
+        <ProjectCard
+          title="Faveart"
+          logo="/Images/FaveartLogo.png"
+          github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+          description="senior capstone"
+        ></ProjectCard>
+
+        <ProjectCard
+          title="Faveart"
+          logo="/Images/TravelBlogLogo.png"
+          github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+          description="senior capstone"
+        ></ProjectCard>
+      </ProjectDeck>
     </>
   )
 }
