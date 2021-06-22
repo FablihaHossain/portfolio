fd80f5a43effac7265fcbf7332c82e624faae8a1
Fabliha Hossain <fablihahossain27@gmail.com>
2021-06-21 23:35:46 -0400

    updated project card component and tested

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText, CardImg, CardFooter } from 'reactstrap';
 
-const MyCard = styled(Card)({
-    height: '300px',
-    width: '300px'
+const MyCard = styled(Card)((props) => {
+    const { updatecard } = props;
+    return {
+        height: updatecard ? '275px' : '375px',
+        width: updatecard ? '275px' : '375px',
+        display: 'flex',
+        marginBottom: '170px !important'
+    }
 });
 
 const TheBody = styled(CardBody)({
-    backgroundColor: 'skyblue'
+    backgroundColor: '#ffccb3',
+    minHeight: '150px'
 });
 
-const ProjectCard = ({ title, logo, description, github }) => {
+const LogoImage = styled(CardImg)({
+    width: '100%',
+    height: '100%'
+});
+
+const ProjectTitle = styled(CardTitle)({
+    color: '#b366ff',
+    textAlign: 'center'
+});
+
+const TextContainer = styled(CardText)({
+    height: 'calc(100% - 75px)'
+});
+
+const TheFooter = styled(CardFooter)({
+    textAlign: 'center',
+    backgroundColor: 'transparent',
+})
+
+const ProjectCard = ({ title, logo, description, github, sidenavopen }) => {
+    const [changeCard, setChangeCard] = useState(sidenavopen)
+    // const changeCardToggle = () => setChangeCard(sidenavopen)
+
+    // console.log('sidenavopen', sidenavopen)
+    // useEffect(() => {
+    //     if (changeCard !== sidenavopen) {
+    //         setChangeCard(sidenavopen)
+    //     }
+    // }, [sidenavopen])
+    // console.log('changeCard', changeCard)
     return (
-        <MyCard>
-            <CardImg top src={logo} width='100%' height='100%'></CardImg>
+        <MyCard updatecard={sidenavopen}>
+            <LogoImage top src={logo}></LogoImage>
             <TheBody>
-                <CardTitle tag="h5">{title}</CardTitle>
-                <CardText tag="h6">{description}</CardText>
-                <CardText>
-                    <a href={github}> Project Link</a>
-                </CardText>
+                <ProjectTitle tag="h5"><i>{title}</i></ProjectTitle>
+                <TextContainer tag="h6"> <b>Description:</b> {description}</TextContainer>
+                <TheFooter>
+                    <a href={github}> Check Out Project</a>
+                </TheFooter>
             </TheBody>
         </MyCard>
     )
--- pages/projects.js
+++ pages/projects.js
@@ -1,37 +1,107 @@
 import React from 'react';
 import styled from '@emotion/styled'
 import ProjectCard from '../components/ProjectCard'
-import { Row } from 'reactstrap'
+import { CardDeck } from 'reactstrap'
 
-const ProjectDeck = styled(Row)({
-    display: 'grid',
-    gridTemplateColumns: 'auto auto auto',
-    padding: '70px'
+const ProjectDeck = styled(CardDeck)({
+    // display: 'flex',
+    // flexDirection: 'column',
+    // width: '100%'
+    '@media (min-width: 700px)': {
+        display: 'grid',
+        gridTemplateColumns: 'auto auto auto',
+        paddingTop: '80px',
+        paddingLeft: '80px'
+    },
+    '@media (max-width: 699px)': {
+        display: 'grid',
+        gridTemplateColumns: 'auto',
+        paddingTop: '80px',
+        paddingLeft: '80px'
+    },
 });
 
-export default function Projects() {
+const RowContainer = styled.div({
+    display: 'flex'
+})
+export default function Projects({ sideNavOpen }) {
     return (
-        <ProjectDeck>
-            <ProjectCard
-                title="Faveart"
-                logo="/Images/FaveartLogo.png"
-                github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
-                description="senior capstone"
-            ></ProjectCard>
+        <>
+            <ProjectDeck>
 
-            <ProjectCard
-                title="Faveart"
-                logo="/Images/FaveartLogo.png"
-                github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
-                description="senior capstone"
-            ></ProjectCard>
+                <ProjectCard
+                    title="FAVEART For OnWords"
+                    logo="/Images/FaveartLogo.png"
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="My Senior Capstone"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
 
-            <ProjectCard
-                title="Faveart"
-                logo="/Images/TravelBlogLogo.png"
-                github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
-                description="senior capstone"
-            ></ProjectCard>
-        </ProjectDeck>
+                <ProjectCard
+                    title="TahiyaClick"
+                    logo=""
+                    github="https://github.com/FablihaHossain/tahiya_click"
+                    description="A Photography site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="TravelBlog"
+                    logo="/Images/TravelBlogLogo.png"
+                    github="https://github.com/FablihaHossain/travel_blog"
+                    description="practice site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="FAVEART For OnWords"
+                    logo="/Images/FaveartLogo.png"
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="My Senior Capstone"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="TahiyaClick"
+                    logo=""
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="A Photography site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="TravelBlog"
+                    logo="/Images/TravelBlogLogo.png"
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="practice site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="FAVEART For OnWords"
+                    logo="/Images/FaveartLogo.png"
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="My Senior Capstone"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="TahiyaClick"
+                    logo=""
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="A Photography site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+                <ProjectCard
+                    title="TravelBlog"
+                    logo="/Images/TravelBlogLogo.png"
+                    github="https://github.com/FablihaHossain/FAVEART-For-OnWords"
+                    description="practice site"
+                    sidenavopen={sideNavOpen}
+                ></ProjectCard>
+
+            </ProjectDeck>
+        </>
     )
 }
