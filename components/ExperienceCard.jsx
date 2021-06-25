a05d30dd44b49ea472c78840e478d76e3e8c0c60
Fabliha Hossain <fablihahossain27@gmail.com>
2021-06-24 22:28:20 -0400

    updated experience card component and tested it

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -1,18 +1,74 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { Button, Collapse, Card, CardBody } from 'reactstrap';
+import { Button, Collapse, Card, CardBody, CardTitle, CardFooter, Row } from 'reactstrap';
 
-export default function ExperienceCard({ job, details }) {
+const StyledCard = styled(Card)({
+    marginBottom: '20px',
+    backgroundColor: '#ffccff',
+    boxShadow: '8px 28px 50px rgba(39,44,49,.07), 1px 6px 12px rgba(39,44,49,.10)',
+    transition: 'all .5s ease',
+    '&:hover': {
+        transform: 'translate3D(0, -1px, 0) scale(1.03)',
+        boxShadow: '5px 10px 44px #26004d, -5px -10px 44px #ff8080',
+        transition: 'all .4s ease',
+    }
+});
+
+const WorkTitle = styled(CardTitle)({
+    textAlign: 'left',
+    fontSize: '20px',
+    marginBottom: '0px',
+    fontFamily: 'Newsreader, serif'
+});
+
+const StyledBody = styled(CardBody)({
+
+});
+
+const Description = styled.p({
+    fontSize: '15px',
+    padding: '0.50rem',
+    fontFamily: 'Newsreader, serif'
+});
+
+const StyledFooter = styled(CardFooter)({
+    color: '#6c757d',
+    padding: '0.75rem 1.25rem',
+    backgroundColor: 'transparent',
+    textAlign: 'right'
+});
+
+const TheDurationText = styled.span({
+    color: '#6c757d'
+});
+
+const Header = styled.div({
+    display: 'flex',
+    justifyContent: 'space-between'
+});
+
+export default function ExperienceCard({ job, details, duration }) {
     const [open, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!open);
     return (
         <>
-            <Button onClick={toggle}> {job} </Button>
-            <Collapse isOpen={open} >
-                <Card>
-                    <CardBody>{details}</CardBody>
-                </Card>
-            </Collapse>
+            {/* <Button onClick={toggle}> {job} </Button>
+            <Collapse isOpen={open} > */}
+            <StyledCard>
+                <WorkTitle>
+                    <Header>
+                        <span><b>{job}</b></span>
+                        <TheDurationText>{duration}</TheDurationText>
+                    </Header>
+                </WorkTitle>
+                <StyledBody>
+                    <Description> {details}</Description>
+                </StyledBody>
+                <StyledFooter>
+                    {duration}
+                </StyledFooter>
+            </StyledCard>
+            {/* </Collapse> */}
         </>
     )
 }
--- pages/experience.js
+++ pages/experience.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { Jumbotron } from 'reactstrap';
 import ExperienceCard from '../components/ExperienceCard';
+
+const Header = styled.p({
+    fontFamily: 'Bungee Inline, cursive',
+    fontSize: '50px',
+    textAlign: 'center'
+});
+
+const Jumbo = styled(Jumbotron)({
+    backgroundColor: 'transparent',
+    paddingTop: '20px'
+});
+
 
 export default function Experience() {
     return (
         <>
-            <ExperienceCard job={'the first job'} details={'blah'}></ExperienceCard>
-            <ExperienceCard job={'the second job'} details={'blah blah'}></ExperienceCard>
-            <ExperienceCard job={'the third job'} details={'blah blah blah'}></ExperienceCard>
+            <Header> Work Experience</Header>
+            <Jumbo>
+                <ExperienceCard
+                    job={'Epistem Interactive'}
+                    details={'blah'}
+                    duration={'2020-present'}
+                ></ExperienceCard>
+                <ExperienceCard
+                    job={'Adelphi University IT Help Desk'}
+                    details={'blah blah'}
+                    duration={'2018-2020'}
+                ></ExperienceCard>
+                <ExperienceCard
+                    job={'Math and Computer Science Teaching Assistant'}
+                    details={'blah blah blah'}
+                    duration={'2018-2020'}
+                ></ExperienceCard>
+            </Jumbo>
         </>
     )
 }
